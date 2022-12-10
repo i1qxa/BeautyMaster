49174ce50711145e8a0e89c3519dac63586f99d8
i1qxa <kuiv1989@gmail.com>
2022-12-10 22:39:32 +0300

    Delete UnitOfMeasurement. Add field unitOM in MaterialItem.kt

--- app/src/main/java/com/example/beautymaster/domain/materials/MaterialItem.kt
+++ app/src/main/java/com/example/beautymaster/domain/materials/MaterialItem.kt
@@ -3,4 +3,5 @@ package com.example.beautymaster.domain.materials
 data class MaterialItem(
     val id : Int,
     val name : String,
+    val unitOM : String,
 )
--- app/src/main/java/com/example/beautymaster/domain/unitOfMeasurement/UnitOMItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.beautymaster.domain.unitOfMeasurement
-
-data class UnitOMItem(
-    val id : Int,
-    val baseUnitOMId : Int?,
-    val multiplayer : Int?,
-    val fullName : String,
-    val shortName : String,
-)
